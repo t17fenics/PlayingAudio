5c6d4287d68ae62a091ccaaaa76b4b9953428f1e
t17fenics <t17fenics@gmail.com>
2020-08-03 10:25:00 +0300

    Audio Playing

--- app/src/main/java/com/example/playingaudio/MainActivity.java
+++ app/src/main/java/com/example/playingaudio/MainActivity.java
@@ -34,6 +34,7 @@ public class MainActivity extends AppCompatActivity {
                 audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, progress, 0);
             }
 
+
             @Override
             public void onStartTrackingTouch(SeekBar seekBar) {
 
