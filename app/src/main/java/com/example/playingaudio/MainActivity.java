e92259c84454cfc1de8b0c42ff8911e5561647a2
t17fenics <t17fenics@gmail.com>
2020-08-03 03:35:05 +0300

    volumeSeekBar

--- app/src/main/java/com/example/playingaudio/MainActivity.java
+++ app/src/main/java/com/example/playingaudio/MainActivity.java
@@ -2,35 +2,70 @@ package com.example.playingaudio;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.MediaController;
+import android.widget.SeekBar;
 
 public class MainActivity extends AppCompatActivity {
     MediaPlayer mediaPlayer;
     Button playButton;
+    AudioManager audioManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        audioManager = (AudioManager)getSystemService(AUDIO_SERVICE);
+        int maxVolume = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+       audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, 0, 0);
+
+        SeekBar volumeSeekBar = findViewById(R.id.volumeSeekBar);
+        volumeSeekBar.setMax(maxVolume);
+        volumeSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                Log.d("seekBar", "" + progress);
+                audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, progress, 0);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
         mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.stuff);
         playButton = (Button)findViewById(R.id.playButton);
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(mediaPlayer.isPlaying()) {
+                    pause();
+                } else {
+                    play();
+                }
+            }
+        });
+    }
+
+    public void play() {
+        playButton.setText("Pause");
         mediaPlayer.start();
     }
 
-    public void play(View view) {
-        if(playButton.getText().toString().equals("Play")) {
-            mediaPlayer.pause();
-            playButton.setText("Pause");
-        } else if(playButton.getText().toString().equals("Pause")){
-            playButton.setText("Play");
-            mediaPlayer.start();
-        }
+    public void pause() {
+        mediaPlayer.pause();
+        playButton.setText("Play");
     }
-
-
 }
